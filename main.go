f5d58aa3db83d0f250506138e6e10a68297718a0
Smliexx <msmliexx1@gmail.com>
2023-02-24 15:34:16 +0800

    fix args error
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- main.go
+++ main.go
@@ -5,17 +5,24 @@ import (
 	"openai-cli-go/client"
 	"openai-cli-go/config"
 	"os"
+	"strings"
 )
 
 func main() {
 	args := os.Args
-	if len(args) != 2 {
+	if len(args) < 2 {
 		panic("not found query param!")
+	}
+	
+	var query strings.Builder
+
+	for i := 1; i < len(args); i++ {
+		query.WriteString(args[i])
 	}
 
 	openAiInfo := config.NewOpenAIInfo("application.yml")
 	openAIClient := client.NewOpenAIClient(openAiInfo)
-	answers, err := openAIClient.Query(args[1])
+	answers, err := openAIClient.Query(query.String())
 
 	if err != nil {
 		panic(err)
